52af34934d0fff1f705d95410f9c553fc92eae2e
William Mairs <wwmairs@gmail.com>
2017-04-22 16:17:51 -0400

    working server, running on 80

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// from https://expressjs.com/en/api.html#app.listen
 var express = require('express');
+var http = require('http');
+var app = express();
 
 var bodyParser = require('body-parser');
-var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
 // from https://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+http.createServer(app).listen(80);
+// https.createServer(options, app).listen(443);
