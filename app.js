c04da1db76a3f0f7d575bed288e85474e737ce12
William Mairs <wwmairs@gmail.com>
2017-04-22 10:43:51 -0400

    forgot to save app.js

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
+
+// from https://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
