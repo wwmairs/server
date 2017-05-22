d274f81376c7048734c214fff1de0d45826752db
will <wwmairs@gmail.com>
2017-05-22 11:59:03 -0400

    added simple run script

--- app.js
+++ app.js
@@ -13,16 +13,17 @@ app.use(bodyParser.urlencoded({ extended : true}));
 app.get('/weather.json', function(request, response) {
         var forecastIo = new ForecastIo('b98bd842e0894e2f05cb3bc94579718c');
         forecastIo.forecast('42.402', '-71.126').then(function(data) {
+                response.setHeader('Access-Control-Allow-Origin', '*');
                 response.send(JSON.stringify(data, null, 2));
         });
 });
 
 // from https://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('/home/ubuntu/Desktop/wwmairs'));
 
 
 http.createServer(app).listen(80);
-// https.createServer(options, app).listen(443);
+//https.createServer(options, app).listen(443);
 
 
 // public IP of the pine is 24.61.43.116
