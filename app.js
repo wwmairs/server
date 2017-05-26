029f29ad7c36d5800d7fe23c56d4d24e80f2a429
wwmairs <wwmairs@gmail.com>
2017-05-26 13:13:40 -0400

    debugging db

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.get('/weather.json', function(request, response) {
 // }
 
 app.get('/sun.json', function(request, response) {
-    db.del('sun_info');
     var curr_date = (new Date).getDate();
     if (!db.has('sun_info') ||
         (db.get('sun_info').date != curr_date)) {
